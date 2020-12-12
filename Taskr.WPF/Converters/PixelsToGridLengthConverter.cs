d60cdec519147bfeeb5ec94c084a2876b69f2be5
Jimmy Satterfield <fedora.game.dev@gmail.com>
2020-12-11 21:22:48 -0800

    Added Modern Title Bar

---
+++ Taskr.WPF/Converters/PixelsToGridLengthConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Taskr.WPF.Converters
+{
+    public class PixelsToGridLengthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => new GridLength((double)value);
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+}
